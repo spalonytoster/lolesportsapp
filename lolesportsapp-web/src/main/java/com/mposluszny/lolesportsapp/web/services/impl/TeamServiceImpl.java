f3a77ac1a4fc69f12188ebb15db0e1b5937a542d
Maciej Posłuszny <xmachiei@gmail.com>
2017-03-30 20:49:49 +0200

    Update TeamServiceImpl.java

--- lolesportsapp-web/src/main/java/com/mposluszny/lolesportsapp/web/services/impl/TeamServiceImpl.java
+++ lolesportsapp-web/src/main/java/com/mposluszny/lolesportsapp/web/services/impl/TeamServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mposluszny.lolesportsapp.core.dao.DAOManager;
-import com.mposluszny.lolesportsapp.core.dao.DaoManagerTest;
 import com.mposluszny.lolesportsapp.core.dao.PlayerDao;
 import com.mposluszny.lolesportsapp.core.dao.TeamDao;
 import com.mposluszny.lolesportsapp.core.model.Player;
